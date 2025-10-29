264e10031a1f7abc7a196ef4538f2ca5ca087e5f
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-29 23:38:25 +0530

    Integrate socket.io-client for real-time user connection updates in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,42 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 function App() {
   const [youtubeUrl, setYoutubeUrl] = useState("");
+  const [connectedUsers, setConnectedUsers] = useState([]);
   console.log(youtubeUrl);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected to server");
+      socket.on("users", (users) => {
+        console.log("users", users);
+        setConnectedUsers(users);
+      });
+    });
+  }, []);
+
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center gap-4">
         <h1 className="text-3xl font-bold text-blue-500 underline">
-          Hello World
+          Hello World {connectedUsers.length} users connected
         </h1>
         <div className="flex gap-2">
-          <Input type="text" placeholder="Enter Youtube url" value={youtubeUrl} onChange={(e) => setYoutubeUrl(e.target.value)} />
+          <Input
+            type="text"
+            placeholder="Enter Youtube url"
+            value={youtubeUrl}
+            onChange={(e) => setYoutubeUrl(e.target.value)}
+          />
           <Button>Start 🎉</Button>
         </div>
-        {youtubeUrl && (
-          <ReactPlayer src={youtubeUrl} />
-        )}
+        {youtubeUrl && <ReactPlayer src={youtubeUrl} />}
       </div>
     </>
   );
