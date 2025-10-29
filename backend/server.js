558438e29484a69efb1c4b33dffefc5450f80436
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-29 23:38:07 +0530

    Add socket.io for real-time communication and update server setup to handle WebSocket connections

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
-import express from 'express';
+// import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// const app = express();
+const httpServer = createServer();
+const connectedUser = new Set();
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
 });
 
-app.listen(3000, () => {
+
+io.on('connection', (socket) => {
+    connectedUser.add(socket.id);
+    console.log('a user connected', socket.id);
+
+    io.emit('users', Array.from(connectedUser));
+    socket.on('disconnect', () => {
+        connectedUser.delete(socket.id);
+        console.log('a user disconnected', socket.id);
+    });
+    socket.on('message', (message) => {
+        console.log('message', message);
+    });
+});
+
+httpServer.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
